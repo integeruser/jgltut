16746bd473a1571bec2c2341599cec850eec5e2c
Francesco Cagnin <francesco.cagnin@gmail.com>
2012-02-15 22:33:28 +0100

    *Renamed HelloTriangle in HelloTriangle01 *Minor changes

--- rosick/mckesson/I/tut01/HelloTriangle01.java
+++ rosick/mckesson/I/tut01/HelloTriangle01.java
@@ -4,6 +4,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL32.*;
 
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
@@ -21,10 +22,10 @@ import rosick.GLWindow;
  * http://www.arcsynthesis.org/gltut/Basics/Tutorial%2001.html
  * @author integeruser
  */
-public class HelloTriangle extends GLWindow {
+public class HelloTriangle01 extends GLWindow {
 	
 	public static void main(String[] args) {
-		new HelloTriangle().start();
+		new HelloTriangle01().start();
 	}
 
 	
@@ -32,33 +33,33 @@ public class HelloTriangle extends GLWindow {
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	
+	private final float vertexPositions[] = {
+			0.75f,  0.75f, 0.0f, 1.0f,
+			0.75f, -0.75f, 0.0f, 1.0f,
+		   -0.75f, -0.75f, 0.0f, 1.0f,
+	};
+
+	private final String strVertexShader = 
+			"#version 330 \n" +
+					"\n" +
+					"layout(location = 0) in vec4 position;\n" +
+					"void main()\n" +
+					"{\n" +
+					"    gl_Position = position;\n" +
+					"}";
+
+	private final String strFragmentShader = 
+			"#version 330\n" +
+					"\n" +
+					"out vec4 outputColor;\n" +
+					"void main()\n" +
+					"{\n" +
+					"   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n" +
+					"}";
+		
 	private int theProgram;
 	private int positionBufferObject;
 	private int vao;
-	
-	private final float vertexPositions[] = {
-		 0.75f,  0.75f, 0.0f, 1.0f,
-		 0.75f, -0.75f, 0.0f, 1.0f,
-		-0.75f, -0.75f, 0.0f, 1.0f,
-	};
-	
-	private final String strVertexShader = 
-		"#version 330 \n" +
-		"\n" +
-		"layout(location = 0) in vec4 position;\n" +
-		"void main()\n" +
-		"{\n" +
-		"    gl_Position = position;\n" +
-		"}";
-		
-	private final String strFragmentShader = 
-		"#version 330\n" +
-		"\n" +
-		"out vec4 outputColor;\n" +
-		"void main()\n" +
-		"{\n" +
-		"   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n" +
-		"}";
 
 	
 
@@ -78,13 +79,13 @@ public class HelloTriangle extends GLWindow {
 	}
 	
 	private void initializeVertexBuffer() {
-		FloatBuffer tempBuffer = BufferUtils.createFloatBuffer(vertexPositions.length);
-		tempBuffer.put(vertexPositions);
-		tempBuffer.flip();
+		FloatBuffer vertexPositionsBuffer = BufferUtils.createFloatBuffer(vertexPositions.length);
+		vertexPositionsBuffer.put(vertexPositions);
+		vertexPositionsBuffer.flip();
         
 		positionBufferObject = glGenBuffers();	       
 		glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
-	    glBufferData(GL_ARRAY_BUFFER, tempBuffer, GL_STATIC_DRAW);
+	    glBufferData(GL_ARRAY_BUFFER, vertexPositionsBuffer, GL_STATIC_DRAW);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
@@ -121,12 +122,32 @@ public class HelloTriangle extends GLWindow {
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	
-	private int createShader(int shaderType, String strShaderFile) {
-        int shader = glCreateShader(shaderType);
-        
-        if (shader != 0) {            
-            glShaderSource(shader, strShaderFile);
-            glCompileShader(shader);
+	private int createShader(int eShaderType, String strShaderFile) {
+        int shader = glCreateShader(eShaderType);
+        glShaderSource(shader, strShaderFile);
+
+        glCompileShader(shader);
+
+        int status = glGetShader(shader, GL_COMPILE_STATUS);
+        if (status == 0) {
+    		int infoLogLength = glGetShader(shader, GL_INFO_LOG_LENGTH);
+
+    		String strInfoLog = glGetShaderInfoLog(shader, infoLogLength);
+
+    		String strShaderType = null;
+			switch (eShaderType) {
+			case GL_VERTEX_SHADER:
+				strShaderType = "vertex";
+				break;
+			case GL_GEOMETRY_SHADER:
+				strShaderType = "geometry";
+				break;
+			case GL_FRAGMENT_SHADER:
+				strShaderType = "fragment";
+				break;
+			}
+
+    		System.err.printf("Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
         }
         
 		return shader;
@@ -134,21 +155,28 @@ public class HelloTriangle extends GLWindow {
 	
 	
 	private int createProgram(ArrayList<Integer> shaderList) {		
-	    int program = glCreateProgram();
-	    
-	    if (program != 0) {
-		    for (Integer shader : shaderList) {
-		    	glAttachShader(program, shader);
-			}
-		    	    
-		    glLinkProgram(program);
-	        glValidateProgram(program);
-	        
-		    for (Integer shader : shaderList) {
-		    	glDetachShader(program, shader);
-			}
-	    }
-	    
-	    return program;
+		int program = glCreateProgram();
+
+		for (Integer shader : shaderList) {
+			glAttachShader(program, shader);
+		}
+
+		glLinkProgram(program);
+		
+		int status = glGetProgram(program, GL_LINK_STATUS);
+		if (status == 0) {
+			int infoLogLength = glGetProgram(program, GL_INFO_LOG_LENGTH);
+
+			String strInfoLog = null;
+			strInfoLog = glGetProgramInfoLog(program, infoLogLength);
+			
+			System.err.printf("Linker failure: %s\n", strInfoLog);
+		}
+		
+		for (Integer shader : shaderList) {
+			glDetachShader(program, shader);
+		}
+
+		return program;
 	}
 }
